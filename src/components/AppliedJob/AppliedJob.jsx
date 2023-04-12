2305fa0cdcc1aaefb5f20bc3f6be10e0ea430aae
habib-N19 <habiburnabiarafat@gmail.com>
2023-04-12 16:51:15 +0600

    added filter option on applied jobs

--- src/components/AppliedJob/AppliedJob.jsx
+++ src/components/AppliedJob/AppliedJob.jsx
@@ -1,41 +1,54 @@
 import { FeaturedContext } from '../../App'
-import React, { useContext } from 'react'
-import { useLoaderData } from 'react-router-dom'
-import { featuredCardData } from '../../loaders/getFeaturedData'
+import React, { useContext, useEffect, useState } from 'react'
 import { getStoredId } from '../../utils/fakeDb'
-import { key } from 'localforage'
+
 import SingleAppliedJob from '../singleAppliedJob/SingleAppliedJob'
 
 const AppliedJob = () => {
   const featuredData = useContext(FeaturedContext)
-  // console.log(featuredData)
   const storedIdData = getStoredId()
-  // console.log(storedIdData)
   const matchedData = []
   for (const id in storedIdData) {
     const foundJobId = featuredData.find(strIds => strIds.id === id)
-    // console.log(foundJobId)
     matchedData.push(foundJobId)
   }
-  // console.log(matchedData)
+  const [filterData, setFilter] = useState(matchedData)
+  const [selectedValue, setSelectedValue] = useState(null)
 
-  // getStoredId()
-  // step-1 setting a loader,
-  // step -2 find match id with get local storage id
-  //push them into an array
-  // putting it to a loader
-  /**
-   *destructing it to the applied job list
+  const handleFilterChange = event => {
+    setSelectedValue(event.target.value)
+  }
 
-   */
-
-  // const storedJobId = getAppliedList()
-  // console.log(storedJobId)
+  useEffect(() => {
+    let filteredData = matchedData
+    if (selectedValue === 'Remote') {
+      filteredData = matchedData.filter(jb => jb.remoteOrOnsite === 'Remote')
+    } else if (selectedValue === 'Onsite') {
+      filteredData = matchedData.filter(jb => jb.remoteOrOnsite === 'Onsite')
+    }
+    if (JSON.stringify(filteredData) !== JSON.stringify(filterData)) {
+      setFilter(filteredData)
+    }
+  }, [matchedData, selectedValue, filterData])
 
   return (
-    <div>
-      from applied jobs
-      {matchedData.map(job => (
+    <div className=''>
+      <div className='flex justify-end w-11/12 mx-auto'>
+        <select
+          className='  text-default-color p-3 bg-gray-100 font-semibold rounded'
+          value={selectedValue}
+          onChange={handleFilterChange}
+        >
+          <option value='' selected disabled>
+            Filter By
+          </option>
+          <option value=''>See All</option>
+          <option value='Remote'>Remote</option>
+          <option value='Onsite'>Onsite</option>
+        </select>
+      </div>
+
+      {filterData.map(job => (
         <SingleAppliedJob key={job.id} job={job}></SingleAppliedJob>
       ))}
     </div>
