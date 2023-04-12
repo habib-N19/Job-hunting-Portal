9401d957e2893b1f6a6f3d237245d9a4cc288b09
habib-N19 <habiburnabiarafat@gmail.com>
2023-04-12 23:55:31 +0600

    fixed header responive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,13 +6,13 @@ const Header = () => {
   const location = useLocation()
   return (
     <div className='banner w-11/12 mx-auto mb-8'>
-      <div className=' relative flex my-6 items-center justify-between w-11/12 mx-auto z-20'>
+      <div className=' relative flex flex-col sm:flex-row my-6 items-center justify-between w-11/12 mx-auto z-20'>
         <NavLink to='/'>
           <h1 className=' animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-3xl sm:2xl font-black text-transparent'>
             Job Hunting Portal
           </h1>
         </NavLink>
-        <ul className='inline-flex gap-4 ml-4 font-semibold '>
+        <ul className='flex flex-col sm:flex-row gap-4 ml-4 font-semibold '>
           {/* not rendering home icon in home */}
           {location.pathname !== '/' && (
             <NavLink
